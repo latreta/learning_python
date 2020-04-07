76432210303f7af1d2b4717257a7e63e74a367f3
latreta <yves_henry13@hotmail.com>
2020-04-07 15:57:31 -0300

    Sets

--- Module 1 - Learning Basics/sets.py
+++ Module 1 - Learning Basics/sets.py
@@ -1,1 +1,19 @@
 # Set is a collection which is unordered and unindexed. No duplicate members.
+vehicles = {"Car", "Motorcycle", "Boat"}
+print(vehicles)
+
+def checkIfExists(vehicle, vehiclesSet): 
+    print(vehicle in vehiclesSet)
+
+def addVehicle(vehicle, vehiclesSet):
+    vehiclesSet.add(vehicle)
+
+def addVehicles(items, vehiclesSet):
+    vehiclesSet.update(items)
+
+def removeVehicle(vehicle, vehiclesSet):
+    vehiclesSet.discard(vehicle) # same from remove but doesn't raise error if dont exist
+
+def createSet():
+    mySet = set(("Red", "Blue", "Green"))
+    print(mySet)
