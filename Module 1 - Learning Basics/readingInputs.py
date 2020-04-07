39d7b8b381522e4d9d3246e06911525d242c3692
latreta <yves_henry13@hotmail.com>
2020-04-07 14:23:14 -0300

    Reading and Casting Inputs, testing For loops

---
+++ Module 1 - Learning Basics/readingInputs.py
@@ -0,0 +1,4 @@
+nome = str(input("Qual seu nome? "))
+idade = int(input("Digite a sua idade: "))
+
+print(f"Seu nome é {nome}\nSua idade é {idade}")
---
+++ Module 1 - Learning Basics/tryingLoops.py
@@ -0,0 +1,13 @@
+print("From 0 to less than 6")
+for x in range(6):
+    print(x)
+
+
+print("From 2 to less than 6")
+for x in range(2, 6):
+    print(x)
+
+
+print("From 2 to less than 30 adding 3 at each loop")
+for x in range(2, 30, 2):
+  print(x)
