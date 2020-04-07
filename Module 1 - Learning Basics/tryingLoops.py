f7a6e74c3676a742c381b927208dee55d3ceb8ce
latreta <yves_henry13@hotmail.com>
2020-04-07 15:11:13 -0300

    Loops done

--- Module 1 - Learning Basics/tryingLoops.py
+++ Module 1 - Learning Basics/tryingLoops.py
@@ -1,37 +1,73 @@
-myString = "TESTING"
+def displayMainMenuFunction():
+    print("Welcome, what would you like to do? ")
+    print("1 - Loop through all character from a String.")
+    print("2 - Loop through array from 0 to Number.")
+    print("3 - Loop from X to Y")
+    print("4 - Loop from X to Y with new increase rate")
+    print("5 - Loop using While using break")
+    print("6 - Fill array with names.")
+    print("0 - Sair do programa.")
 
-print("Looping through each Character from String")
-# for x in myString:
-#     print(x)
+def handleUserInput():
+    opcao = int(input("Digite sua opcao: "))
+    if opcao == 0:
+        pass
+    elif opcao == 1:
+        loopThroughCharactersString("TESTING")
+        pass
+    elif opcao == 2:
+        loopThroughArrayLessThan(6)
+        pass
+    elif opcao == 3:
+        loopFromXtoY(0, 6)
+        pass
+    elif opcao == 4:
+        loopFromXToYIncremented(2, 31, 2)
+        pass
+    elif opcao == 5:
+        loopingThroughWhile(15, 10)
+        pass
+    elif opcao == 6:
+        fillArrayUsingInput()
+        pass
 
-print("From 0 to less than 6")
-# for x in range(6):
-#     print(x)
+def loopThroughCharactersString(myText):
+    print(f"Looping through each Character from String {myText}")
+    for x in myText:
+        print(x)
 
+def loopThroughArrayLessThan(end):
+    print(f"From 0 to less than {end}")
+    for x in range(end):
+        print(x)
 
-print("From 2 to less than 6")
-# for x in range(2, 6):
-#     print(x)
+def loopFromXtoY(start, end):
+    print(f"From {start} to less than {end}")
+    for x in range(start, end):
+        print(x)
 
+def loopFromXToYIncremented(start, end, incrementBy):
+    print(f"From {start} to less than {end} adding {incrementBy} at each loop")
+    for x in range(start, end, incrementBy):
+        print(x)
 
-print("From 2 to less than 30 adding 3 at each loop")
-# for x in range(2, 30, 2):
-#    print(x)
+def loopingThroughWhile(end, destinationToBreak):
+    i = 1
+    print(f"Using While Loops, looping from {i} to less than {end}")
+    while i < end:
+        print(i)
+        if i == destinationToBreak: # Testing break statement
+            break
+        i += 1
 
-i = 1
-length = 10
-print(f"Using While Loops, looping from {i} to less than {length}")
-while i < length:
-    print(i)
-    if i == 15: # Testing break statement
-        break
-    i += 1
+def fillArrayUsingInput():
+    names = []
+    opcao = 1
+    while opcao == 1:
+        name = str(input("Digite um nome: "))
+        names.append(name)
+        opcao = int(input("Se quiser adicionar mais, digite 1, caso contrário, digite 0: "))
+    print(names)
 
-
-names = []
-opcao = 1
-while opcao == 1:
-    name = str(input("Digite um nome: "))
-    names.append(name)
-    opcao = int(input("Se quiser adicionar mais, digite 1, caso contrário, digite 0: "))
-print(names)
+displayMainMenuFunction()
+handleUserInput()
