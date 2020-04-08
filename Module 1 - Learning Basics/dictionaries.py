20d6146f981c06511830f325de139aaa7f429b29
latreta <yves_henry13@hotmail.com>
2020-04-08 13:38:31 -0300

    Dictionaries

--- Module 1 - Learning Basics/dictionaries.py
+++ Module 1 - Learning Basics/dictionaries.py
@@ -1,1 +1,29 @@
 # Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
+person = {
+  "name": "Yves",
+  "age": 26,
+  "gender": "male"
+}
+
+def displayValues(user):
+    for x in user:
+        print(f"{x}: {user[x]}")
+
+def displayBoth(user):
+    for x,y in user.items():
+        print(x, y)
+
+
+def doesKeyExists(keyName, user):
+    if keyName in user:
+        print("True")
+
+def addNewKey(keyName, keyValue, user):
+    user[keyName] = keyValue
+
+def removeKey(keyName, user):
+    user.pop(keyName)
+
+def clearDictionary(user):
+    user.clear()
